56c3d8a547b0bb4686a8b435c5dea76589d98b28
andrei-vilcan <62720457+andrei-vilcan@users.noreply.github.com>
2021-07-02 10:57:27 -0400

    algo

---
+++ algorithm.py
@@ -0,0 +1,14 @@
+from stock import Stock
+import time
+
+stock_dict = []
+
+# def zero():
+#     for stock in stock:
+#
+
+def one(stock, time, limit):
+    for time in stock.dates:
+        time = 0
+        while time > limit:
+            
